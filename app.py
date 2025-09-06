2935b1fc72850837d89b404e34911710390d2d86
Riniii09 <rinippillai@gmail.com>
2025-09-06 14:29:44 +0530

    image fix

--- app.py
+++ app.py
@@ -251,6 +251,7 @@ def add_product():
 @app.route("/product/<int:product_id>")
 def product_details(product_id):
     product = Product.query.get_or_404(product_id)
+    
     return render_template("product_details.html", product=product)
 
 
